d68d677b60c7a20c369588ee676cc120acab689a
pipatpong1412 <tfghgh.fg@gmail.com>
2024-01-29 12:32:17 +0700

    intitial project

---
+++ index.js
@@ -0,0 +1,8 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT
+
+app.use(express.json())
+
+app.listen(PORT, () => console.log(`THIS APP IS RUNNING ON PORT ${PORT}`))
